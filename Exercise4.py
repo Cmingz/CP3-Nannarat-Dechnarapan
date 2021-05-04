05d97b24a3bd985aa1d2913fac281feff9196b2e
Cmingz <47769491+Cmingz@users.noreply.github.com>
2021-05-04 18:29:45 +0700

    Create Exercise4.py

---
+++ Exercise4.py
@@ -0,0 +1,10 @@
+foundEng = 70
+genBusiness = 70
+introCompSys = 70
+compProgram= 70
+
+print("--- Your Score ---")
+print("Foundation English :", foundEng)
+print("General Business :", genBusiness)
+print("Introduction to Computer System :", introCompSys)
+print("Computer Programming :", compProgram)
